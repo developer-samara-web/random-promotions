1b68a049d3ba8aca020a855afe75121a61c6830f
developer-samara-web <developer.samara.web@gmail.com>
2025-04-07 13:19:19 +0400

    fix request write access

--- keyboards/chanelKeyboard.js
+++ keyboards/chanelKeyboard.js
@@ -4,6 +4,6 @@ import { Markup } from "telegraf";
 // Клавиатура "Участие в акции"
 export function chanelKeyboard(promotion, counter = null) {
     return Markup.inlineKeyboard([
-        [Markup.button.url(`${promotion.requires_subscription ? '🌟 ' : ''}Участвовать ${counter ? `(${counter})` : ''}`, `https://t.me/asfasfsffsafasfsfasfsf_bot?startapp=${promotion._id}`)]
+        [Markup.button.url(`${promotion.requires_subscription ? '🌟 ' : ''}Участвовать ${counter ? `(${counter})` : ''}`, `https://t.me/asfasfsffsafasfsfasfsf_bot?startapp=${promotion._id}&request_write_access=true`)]
     ]);
 }
