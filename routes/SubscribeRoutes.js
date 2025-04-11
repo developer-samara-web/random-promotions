711cb6733731bce1f6babfa9635447dcef9f27fb
developer-samara-web <developer.samara.web@gmail.com>
2025-04-11 12:53:00 +0400

    fix any bugs

--- routes/SubscribeRoutes.js
+++ routes/SubscribeRoutes.js
@@ -11,27 +11,20 @@ export default async function (app, telegram) {
         try {
             // Получаем id акции
             const { telegram_id } = req.body;
-            // Получаем текущий статус подписки
-            const user = await getUser(telegram_id);
-            const status = user?.channel_subscription;
             // Получаем подписку на канал
             const subscribe = await telegram.telegram.getChatMember(process.env.TELEGRAM_GROUP_ID, telegram_id);
-            // Определяем новый статус подписки
-            const newStatus = subscribe.status !== 'left';
-            // Проверяем, изменился ли статус
-            if (status !== newStatus) {
-                // Обновляем данные пользователя только если статус изменился
-                await updateUser(telegram_id, { channel_subscription: newStatus });
-
-                // Логируем изменение
-                if (newStatus) {
-                    logger.info(`Пользователь подписался: ID:${telegram_id}`);
-                } else {
-                    logger.info(`Пользователь отписался: ID:${telegram_id}`);
-                }
-            }
             // Отправляем данные
-            return res.status(200).json({ access: newStatus });
+            if (subscribe.status === 'left') {
+                // Обновляем данные пользователя
+                await updateUser(telegram_id, { channel_subscription: false })
+                logger.info(`Неудачная проверка подписки: ID:${telegram_id}`)
+                return res.status(200).send({ access: false });
+            } else {
+                // Обновляем данные пользователя
+                await updateUser(telegram_id, { channel_subscription: true })
+                logger.info(`Успешная проверка подписки: ID:${telegram_id}`)
+                return res.status(200).json({ access: true });
+            }
         } catch (e) {
             return res.status(403).send(`Ошибка доступа.`);
         }
