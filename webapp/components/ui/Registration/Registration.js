b53710beae3328698dd715242fbe8956793d7aeb
developer-samara-web <developer.samara.web@gmail.com>
2025-11-27 12:48:31 +0400

    hot fix

--- webapp/components/ui/Registration/Registration.js
+++ webapp/components/ui/Registration/Registration.js
@@ -17,35 +17,13 @@ export default function Registration() {
 	return (
 		<Page>
 			<Header title="Регистрация" description="Правила использования бота:" />
-			<Block className="text-center">
-				Перед началом использования бота, пожалуйста, ознакомьтесь с нашими правилами:
-			</Block>
-
 			<Block>
-				<div className="font-medium">1. Наши офф каналы:</div>
-				<div className="flex flex-col gap-1 list-none font-light mt-2">
-					<li>Бесплатные раздачи @Majestic_Ryletka</li>
-					<li>Наш бот @ryletka_garsem_bot</li>
-					<li>Так же есть Приватный канал, в котором самые вкусные раздачи, но доступ туда можно получить через @ryletka_garsem_bot</li>
-					<li>Пруфы всех выплат @pryfi_garsem</li>
+				<div className="flex flex-col gap-3 list-none mt-2 text-sm">
+					<li className="border-b border-[#224684] pb-3">1. Победитель выбирается рандомно через нашего бота.</li>
+					<li className="border-b border-[#224684] pb-3">2. Результаты публикуются в группе и дублируются в ЛС нашим ботом <b>@ryletka_garsem_bot.</b></li>
+					<li className="border-b border-[#224684] pb-3">3. Если победитель не отписал на контакты <b>@gar_sem</b> в течении 3-х дней - разыгрываем приз повторно!</li>
+					<div className="font-medium">По любым вопросам обращаться к @gar_sem</div>
 				</div>
-			</Block>
-
-			<Block>
-				<div className="font-medium">2. Все выплаты производятся ПО ЧЕТВЕРГАМ!</div>
-			</Block>
-
-			<Block>
-				<div className="font-medium">3. Определение победителей:</div>
-				<div className="flex flex-col gap-1 list-none font-light mt-2">
-					<li>3.1. Победитель выбирается рандомно через нашего бота.</li>
-					<li>3.2. Результаты публикуются в группе и дублируются в ЛС нашим ботом  @ryletka_garsem_bot.</li>
-					<li>3.3. Если победитель не отписал на контакты @gar_sem в течении 3-х дней - разыгрываем приз повторно!</li>
-				</div>
-			</Block>
-
-			<Block>
-				<div className="font-medium">4. По любым вопросам обращаться к @gar_sem</div>
 			</Block>
 
 			<div className="sticky bottom-0 left-0 w-full flex bg-gradient-to-b from-transparent to-slate-900 items-center justify-center pt-5">
