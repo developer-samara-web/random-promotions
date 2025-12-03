e94291c086de57230589a8ed9464547728c2ad9f
developer-samara-web <developer.samara.web@gmail.com>
2025-12-03 22:30:11 +0400

    update next config

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: [process.env.NEXT_PUBLIC_URL_NAME],
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lavka-dobbi.fra1.digitaloceanspaces.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+	allowedDevOrigins: [process.env.NEXT_PUBLIC_URL_NAME],
+	poweredByHeader: false,
+	compress: true,
+	swcMinify: true,
+	reactStrictMode: false,
+	trailingSlash: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lavka-dobbi.fra1.digitaloceanspaces.com',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
