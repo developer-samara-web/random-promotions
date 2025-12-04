dbbc3fcf74ec63b51a4d57ccb9b7333f7786e95a
developer-samara-web <developer.samara.web@gmail.com>
2025-12-04 08:35:34 +0400

    update webapp layout

--- webapp/app/layout.js
+++ webapp/app/layout.js
@@ -4,47 +4,27 @@
 import "./globals.css";
 
 // Импорт компонентов
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import { checkAuth } from "@/controllers/Auth";
 import Registration from "@/components/ui/Registration/Registration";
 import Preloader from "@/components/ui/Preloader/Preloader";
-import Page from "@/components/ui/Page/Page";
 import Error from "@/components/ui/Error/Error";
 import Script from 'next/script';
-
-// Список исключенных маршрутов
-const EXCLUDED_ROUTES = [
-	/^\/policy(\/|$)/,
-	/^\/oferta(\/|$)/,
-	/^\/payment(\/|$)/,
-	/^\/payment\/view\//
-];
 
 // Компонент
 export default function RootLayout({ children }) {
 	const [isTelegramScriptLoaded, setIsTelegramScriptLoaded] = useState(false);
-	const [isPaymentScriptLoaded, setIsPaymentScriptLoaded] = useState(false);
 	const [check, setCheck] = useState(null);
 	const [error, setError] = useState(null);
 	const pathname = usePathname();
 
-	// Проверка с использованием регулярных выражений
-	const shouldExclude = useMemo(() => {
-		return EXCLUDED_ROUTES.some(route => route.test(pathname));
-	}, [pathname]);
-
 	useEffect(() => {
 		if (!isTelegramScriptLoaded) return;
-		if (!isPaymentScriptLoaded) return;
 
 		const fetchAuth = async () => {
 			try {
 				if (window.Telegram && Telegram.WebApp) {
-					if (shouldExclude) {
-						setCheck(true);
-						return;
-					}
 					// Инициализация WebApp
 					Telegram.WebApp.ready();
 					// Запрос разрешения на отправку сообщений
@@ -67,7 +47,7 @@ export default function RootLayout({ children }) {
 		};
 
 		fetchAuth();
-	}, [isTelegramScriptLoaded, isPaymentScriptLoaded, pathname, shouldExclude]);
+	}, [isTelegramScriptLoaded, pathname]);
 
 	// Если проверка еще не завершена
 	if (check === null) {
@@ -75,7 +55,6 @@ export default function RootLayout({ children }) {
 			<html lang="ru">
 				<head>
 					<Script src="https://telegram.org/js/telegram-web-app.js" onLoad={() => setIsTelegramScriptLoaded(true)} />
-					<Script src="https://widget.cloudpayments.ru/bundles/cloudpayments.js" onLoad={() => setIsPaymentScriptLoaded(true)} />
 				</head>
 				<body className="root">
 					<Preloader />
@@ -90,7 +69,6 @@ export default function RootLayout({ children }) {
 			<html lang="ru">
 				<head>
 					<Script src="https://telegram.org/js/telegram-web-app.js" onLoad={() => setIsTelegramScriptLoaded(true)} />
-					<Script src="https://widget.cloudpayments.ru/bundles/cloudpayments.js" onLoad={() => setIsPaymentScriptLoaded(true)} />
 				</head>
 				<body className="root">
 					<Error title="Произошла ошибка" description="Произошла ошибка при авторизации. Если проблема повторяется, пожалуйста, свяжитесь с нашей технической поддержкой для уточнения причин." />
@@ -105,7 +83,6 @@ export default function RootLayout({ children }) {
 			<html lang="ru">
 				<head>
 					<Script src="https://telegram.org/js/telegram-web-app.js" onLoad={() => setIsTelegramScriptLoaded(true)} />
-					<Script src="https://widget.cloudpayments.ru/bundles/cloudpayments.js" onLoad={() => setIsPaymentScriptLoaded(true)} />
 				</head>
 				<body className="root">
 					<Registration />
@@ -119,7 +96,6 @@ export default function RootLayout({ children }) {
 		<html lang="ru">
 			<head>
 				<Script src="https://telegram.org/js/telegram-web-app.js" onLoad={() => setIsTelegramScriptLoaded(true)} />
-				<Script src="https://widget.cloudpayments.ru/bundles/cloudpayments.js" onLoad={() => setIsPaymentScriptLoaded(true)} />
 			</head>
 			<body className="root">{children}</body>
 		</html>
